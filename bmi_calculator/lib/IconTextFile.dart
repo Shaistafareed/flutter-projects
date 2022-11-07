a24d5bffa303b6b4cb7df2ae36269c32ae4cb296
Shaistafareed <100860194+Shaistafareed@users.noreply.github.com>
2022-11-07 21:02:11 +0500

    lecture 70

--- bmi_calculator/lib/IconTextFile.dart
+++ bmi_calculator/lib/IconTextFile.dart
@@ -1,1 +1,27 @@
-// TODO Implement this library.
+import 'package:flutter/material.dart';
+import 'constantfile.dart';
+class RpeatTextandICONWidget extends StatelessWidget {
+  RpeatTextandICONWidget({required this.iconData, required this.label});
+
+  late final IconData iconData;
+  late final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          iconData,
+          size: 850,),
+        SizedBox(
+
+        ),
+        Text(
+          label,
+          style: kLabelStyle,
+        ),
+      ],
+    );
+  }
+}
