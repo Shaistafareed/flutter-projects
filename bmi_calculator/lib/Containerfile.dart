02dc8ce133f0110b16c9c028d0eec14c23d6c30a
Shaistafareed <100860194+Shaistafareed@users.noreply.github.com>
2022-11-07 20:25:25 +0500

    lecture 60

---
+++ bmi_calculator/lib/Containerfile.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/material.dart';
+class RepeatContainerCode extends StatelessWidget {
+  RepeatContainerCode({required this.colors , required this.cardWidget, required this.onPressed});
+  final Color colors;
+  final Widget cardWidget;
+  final Function onPressed;
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPressed(),
+      child:Container(
+        margin:EdgeInsets.all(15.0,),
+        child:cardWidget,
+        decoration: BoxDecoration(
+          color:colors,
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+      ),
+    );
+  }
+}
