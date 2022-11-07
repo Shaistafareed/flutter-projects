a084257f71cac33c8cd64388915ae41023680ca7
Shaistafareed <100860194+Shaistafareed@users.noreply.github.com>
2022-11-07 20:19:21 +0500

    lecture 58

--- bmi_calculator/lib/main.dart
+++ bmi_calculator/lib/main.dart
@@ -1,21 +1,17 @@
 import 'package:flutter/material.dart';
-import 'input.dart';
-
+import 'input_page.dart';
+import 'IconTextFile.dart';
+import 'Containerfile.dart';
 void main() => runApp(BMICalculator());
 
 class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: InputPage(),
-      theme: ThemeData.dark().copyWith(
-          primaryColor: Colors.black38,
-          scaffoldBackgroundColor: Colors.black38,
-          textTheme: TextTheme(
-              bodyText1:TextStyle(color:Colors.black))
-      ),
+      theme: ThemeData.dark().copyWith
+        (primaryColor: Color(0xFFA0E21)),
+
     );
   }
 }
-
