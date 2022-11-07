97c3df653acee8af404c80c2fd770328abaa2230
Shaistafareed <100860194+Shaistafareed@users.noreply.github.com>
2022-11-07 20:32:08 +0500

    lecture 62

---
+++ bmi_calculator/lib/constantfile.dart
@@ -0,0 +1,35 @@
+
+import 'package:flutter/material.dart';
+const active = Color(0xFF1D1E33);
+const deactive = Color(0xFF111328);
+const kLabelStyle=TextStyle(
+
+
+  fontSize:18.0,
+  color:Color(0xFF8D8E98),
+
+);
+const kNumberStyle =TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+
+);
+const large=TextStyle(
+  fontSize: 25.0,
+  fontWeight:  FontWeight.bold,
+);
+
+
+
+const style2= TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+const style3= TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+const style4= TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
